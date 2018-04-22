93a117d9a598a12dc3b4156fb57972b76de0bc9a
vcedgar <vcedgar@asu.edu>
2018-04-21 17:46:15 -0700

    Changed the layout and began making the gui

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,17 @@
+package gui;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+	
+	private JButton homeB;
+	private JButton profileB;
+	private JButton charactersB;
+	private JButton communityB;
+	private JButton wikiB;
+	
+	public MainFrame() {
+		
+	}
+
+}
