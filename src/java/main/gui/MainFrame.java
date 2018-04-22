1c4e690e3b55522fdf9aaed24c2dd84df692ef48
zlmonroe <zachary.lynn.monroe@gmail.com>
2018-04-21 18:24:10 -0700

    changed layout for java files

---
+++ src/java/main/gui/MainFrame.java
@@ -0,0 +1,21 @@
+package gui;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+	
+	private JButton homeB;
+	private JButton profileB;
+	private JButton charactersB;
+	private JButton communityB;
+	private JButton wikiB;
+	
+	public MainFrame() {
+		
+	}
+	
+	private void initMain() {
+		
+	}
+
+}
