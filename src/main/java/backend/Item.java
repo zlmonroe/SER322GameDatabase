3495c7a7c7bf57488390708d2277c52ef499ddf3
vcedgar <vcedgar@asu.edu>
2018-04-25 00:38:52 -0700

    forgot to push item

---
+++ src/main/java/backend/Item.java
@@ -0,0 +1,42 @@
+package backend;
+
+public class Item {
+    private String name;
+    private int weight;
+    private double effectMultiplier;
+    
+    private String effect;
+    
+    public Item(String n) {
+    	name = n;
+    	loadItem(n);
+    }
+    
+    public String getName() {
+		return name;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public double getEffectMultiplier() {
+		return effectMultiplier;
+	}
+
+	public String getEffect() {
+		return effect;
+	}
+
+	private boolean loadItem(String n) {
+    	//TODO
+    	
+    	loadEffect();
+    	return false;
+    }
+    
+    private boolean loadEffect() {
+    	//TODO
+    	return false;
+    }
+}
