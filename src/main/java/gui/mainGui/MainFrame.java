bcdf156b4249b4a77a85365347914c6ff0e61bbe
vcedgar <vcedgar@asu.edu>
2018-04-24 20:11:28 -0700

    Better image handling

--- src/main/java/gui/mainGui/MainFrame.java
+++ src/main/java/gui/mainGui/MainFrame.java
@@ -1,9 +1,17 @@
 package gui.mainGui;
 import java.awt.BorderLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import gui.general.ImagePane;
@@ -14,7 +22,6 @@ public class MainFrame extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 	public static final List<String> PANEL_NAMES;
-	private JPanel header;
 	private final static String HEADER_IMG;
 	private MainPanel main;
 	private JPanel buttons;
@@ -26,13 +33,16 @@ public class MainFrame extends JFrame{
 		HEADER_IMG = "src/resources/header.png";
 	}
 	
-	public MainFrame() {
+	public MainFrame() throws IOException {
 		this.setLayout(new BorderLayout());
-		header = new ImagePane(HEADER_IMG, (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), 150);
+		BufferedImage myPicture = ImageIO.read(new File(HEADER_IMG));
+		JLabel picLabel = new JLabel(new ImageIcon(myPicture.getScaledInstance((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth(), 150, Image.SCALE_FAST)));
+		picLabel.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		add(picLabel);
 		main = new MainPanel();
 		buttons = new ButtonPanel(main);
 		
-		this.add(header, BorderLayout.NORTH);
+		this.add(picLabel, BorderLayout.NORTH);
 		this.add(main, BorderLayout.CENTER);
 		this.add(buttons, BorderLayout.WEST);
 		this.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
@@ -41,7 +51,7 @@ public class MainFrame extends JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		MainFrame frame = new MainFrame();
 		frame.setVisible(true);
 	}
