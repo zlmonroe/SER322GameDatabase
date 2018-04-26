8e381bc2353c26a9a47c216150524d10d33951cf
zlmonroe <zachary.lynn.monroe@gmail.com>
2018-04-26 00:48:26 -0700

    resultsettable creator

---
+++ src/main/java/gui/general/ResultSetTable.java
@@ -0,0 +1,43 @@
+package gui.general;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Vector;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * Created by zlmonroe on 4/26/2018.
+ */
+public class ResultSetTable extends JTable {
+    public ResultSetTable(ResultSet resultSet) throws SQLException {
+        this.setModel(buildTableModel(resultSet));
+    }
+
+
+    public static DefaultTableModel buildTableModel(ResultSet rs)
+            throws SQLException {
+
+        ResultSetMetaData metaData = rs.getMetaData();
+
+        // names of columns
+        Vector<String> columnNames = new Vector<String>();
+        int columnCount = metaData.getColumnCount();
+        for (int column = 1; column <= columnCount; column++) {
+            columnNames.add(metaData.getColumnName(column));
+        }
+
+        // data of the table
+        Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+        while (rs.next()) {
+            Vector<Object> vector = new Vector<Object>();
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                vector.add(rs.getObject(columnIndex));
+            }
+            data.add(vector);
+        }
+
+        return new DefaultTableModel(data, columnNames);
+    }
+}
