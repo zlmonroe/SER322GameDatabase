18373fd7f03d31a07701ed0aa2c1b8c69d872152
aqcastan <aqcastan@asu.edu>
2018-04-26 00:03:33 -0700

    Added Update class in SQLActions. Back-end editPlayer method written.

---
+++ src/main/java/backend/sql/SQLActions/Update.java
@@ -0,0 +1,30 @@
+package backend.sql.SQLActions;
+
+public class Update implements SQLAction{
+    private final String tableName;
+    private final String username;
+    private final String attr;
+    private final String newVal;
+
+    public Update(String tableName, String username, String attr, String newVal) {
+        this.tableName = tableName;
+        this.username = username;
+        this.attr = attr;
+        this.newVal = newVal;
+    }
+
+    @Override
+    public String getAction() {
+        if(attr == null || newVal == null) {
+            return "";
+        }
+        return "UPDATE " + tableName + "\n"
+                + "SET " + attr + " = " + newVal + "\n"
+                + "WHERE username = " + username + ";";
+    }
+
+    @Override
+    public String getFunction() {
+        return "Updates attribute " + attr + " value in " + tableName;
+    }
+}
