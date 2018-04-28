42f5d09b3dee4cdd5897aaeb3d9bfb96005eea7d
zlmonroe <zachary.lynn.monroe@gmail.com>
2018-04-27 23:50:06 -0700

    Insert friends action

--- src/main/java/gui/general/SearchField.java
+++ src/main/java/gui/general/SearchField.java
@@ -2,24 +2,18 @@ package gui.general;
 
 import backend.CurrentContext;
 import backend.sql.SQLActions.StringsLike;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import javax.swing.JComboBox;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
 
 /**
  * Created by zlmonroe on 4/27/2018.
  */
-public class SearchField extends JComboBox<String> implements ChangeListener {
+public class SearchField extends JComboBox<String> {
     private List<String> terms = new ArrayList<>();
     private String table;
     private String attribute;
@@ -27,31 +21,50 @@ public class SearchField extends JComboBox<String> implements ChangeListener {
     public SearchField(String table, String attribute) {
         this.table = table;
         this.attribute = attribute;
+        this.addPopupMenuListener(new PopupMenuListener() {
+            @Override
+            public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
+                update();
+            }
+
+            @Override
+            public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
+
+            }
+
+            @Override
+            public void popupMenuCanceled(PopupMenuEvent e) {
+
+            }
+        });
         this.setEditable(true);
+        this.terms = new ArrayList<>();
     }
 
     public void update() {
         System.out.println("UPDATED DROP DOWN");
-        this.terms.addAll(this.search(this.getSelectedItem().toString()));
+        String item = (String)this.getEditor().getItem();
+        System.out.println(item);
+        if (this.getSelectedItem() == null) {
+            item = "";
+        }
+        this.removeAllItems();
+        this.search(item);
     }
 
-    private Collection<String> search(String value) {
-        List<String> found = new ArrayList<>();
+    private void search(String value) {
+        if (value == null) {
+            value = "";
+        }
         ResultSet rs = CurrentContext.gameServer.querry(new StringsLike(this.table, this.attribute,
                 value));
         try {
             while(rs.next()) {
-                found.add(rs.getString(0));
+                this.addItem(rs.getString(1));
+                System.out.println(rs.getString(1));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-        return found;
-    }
-
-    @Override
-    public void stateChanged(ChangeEvent e) {
-        this.update();
     }
 }
